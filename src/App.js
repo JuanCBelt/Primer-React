d57e95dbcda6d76cf749bded58dcc7f992f69c54
Juan Cruz Beltrán <juancruzbelt@gmail.com>
2023-09-04 21:38:05 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="container">
+        <h1>Bienvenido a Styles</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+// components/NavBar.js
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Styles</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Hombre</Nav.Link>
+            <Nav.Link href="#">Mujer</Nav.Link>
+            <Nav.Link href="#">Niños</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
