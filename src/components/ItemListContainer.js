7793a934933f059f94fc41af5657b815773f1bed
JuanCBelt <120066892+JuanCBelt@users.noreply.github.com>
2023-09-20 21:37:23 -0300

    Update ItemListContainer.js

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,8 +4,6 @@ function ItemListContainer({ greeting }) {
   return (
     <div className="container">
       <h1>{greeting}</h1>
-      {/* Agrega aquí el contenido principal de tu tienda */}
-      {/* Puedes agregar secciones de productos, banners, etc. */}
     </div>
   );
 }
